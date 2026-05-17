2df0138583d8c5cb914295ff59527fddb1ef7b0d
Saul Stiven <saulgarcia@unimayor.edu.co>
2026-05-17 11:11:30 -0500

    prueba de actualizacion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();              // creamos la aplicacion
 const port = process.env.PORT || 3000; // usamos el puerto de Render en produccion y 3000 en local
 
 app.get('/', (req, res) => {
-  res.send('Aplicacion v1.0 - CI/CD funcionando'); // respuesta visible para confirmar que la app corre
+  res.send('Aplicacion v2.0 - CI/CD sigue funcionando con render'); // respuesta visible para confirmar que la app corre
 });
 
 app.listen(port, () => console.log(`Servidor ejecutandose en puerto ${port}`)); // levantamos el servidor
